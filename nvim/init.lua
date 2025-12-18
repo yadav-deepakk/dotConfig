62971f1975c6b2f2669e5c0bfe53359f75cb5fc2
yadav-deepakk <deepakyadav53882@gmail.com>
2025-12-18 23:03:46 +0530

    fix: change in keymaps and small refractor in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require"configs.options"
-require"configs.keymaps"
-require"configs.lazy"
-require"configs.autocmds"
+require("configs.options")
+require("configs.keymaps")
+require("configs.lazy")
+require("configs.autocmds")
--- nvim/lua/configs/autocmds.lua
+++ nvim/lua/configs/autocmds.lua
@@ -14,7 +14,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("BufWritePre", {
   group=dky_group,
   pattern="*",
-  command=[[%s/\s\+$//e]],
+  callback = function()
+    vim.cmd [[ %s/\s\+$//e ]]
+  end
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -23,14 +25,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts={ buffer=e.buf }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover({border="rounded"}) end, opts)
-    vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<leader>lsh", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "gws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "gra", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "grr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "grn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "grd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("i", "<c-s>", function() vim.lsp.buf.signature_help() end, opts)
   end
 })
 
