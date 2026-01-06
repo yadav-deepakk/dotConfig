a4fa0756fa462a911c9bc87972bc118ae87a58bc
yadav-deepakk <deepakyadav53882@gmail.com>
2026-01-07 00:05:57 +0530

    fix(ui): add lualine and remove todocomment

--- nvim/lua/plugins/tui.lua
+++ nvim/lua/plugins/tui.lua
@@ -2,14 +2,15 @@ return {
 
   {
     "ellisonleao/gruvbox.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
       require("gruvbox").setup({
         transparent_mode = true,
-        overrides = { Normal = { bg = "NONE" }, }
+        overrides = { Normal = { bg = "NONE" }, },
       })
       vim.cmd [[ colorscheme gruvbox ]]
-    end
+    end,
   },
 
   {
@@ -19,17 +20,31 @@ return {
   },
 
   {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { sign = false },
+    "sphamba/smear-cursor.nvim",
+    opts = {}
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {}
+      },
+    },
   },
 
 }
-
--- FIX: only to test working of todo comment - fix.
--- TODO: only to test working of todo comment - todo.
--- HACK: only to test working of todo comment - hack.
--- WARN: only to test working of todo comment - warning.
--- PERF: only to test working of todo comment - performance.
--- NOTE: only to test working of todo comment - note.
--- TEST: only to test working of todo comment - test.
