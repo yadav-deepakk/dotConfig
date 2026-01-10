1d441fca6807825806f9f5cfb982b581daf0a0dd
yadav-deepakk <deepakyadav53882@gmail.com>
2026-01-10 11:06:25 +0530

    feat(nvim-flutter): add flutter tools in neovim.

---
+++ nvim/lua/plugins/flutter.lua
@@ -0,0 +1,13 @@
+return {
+
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim',
+    },
+    config = true,
+  },
+
+}
