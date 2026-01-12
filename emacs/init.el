882705bc2d34f7f248c2816367e4a9d8be694185
yadav-deepakk <deepakyadav53882@gmail.com>
2026-01-12 23:41:23 +0530

    fix: emacs theme

--- emacs/init.el
+++ emacs/init.el
@@ -73,7 +73,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi-tinted))
+ '(custom-enabled-themes '(tango-dark adwaita))
  '(package-selected-packages '(evil-collection)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
